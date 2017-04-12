4b6b36939ef2fbe4cde5fece12cc0b8898155ad2
Egor Kloos <studio@dutchcelt.com>
2017-04-12 13:07:21 +0200

    Added setTimeout so the last event always fires

--- throttleEvent.js
+++ throttleEvent.js
@@ -2,7 +2,6 @@
  * throttleEvent
  * Used to limit events that can be fired in rapid succession.
  * This can prevent an event like onresize or onscroll from thrashing the UI.
- * NOTE: It will not fire the last event if elapsed time is within delay.
  *
  * Example: addEventListener('resize', throttleEvent(_handler, 500, this), false);
  *
@@ -11,16 +10,22 @@
  * @arg {object} context - Optional scope in which to return the function handler
  * @return {function}    - The timeout function that is triggered by the event listner on each event call
  */
-export const throttleEvent = (func, ms, context = this) => {
+export const throttleEvent = (func, ms, scope = this) => {
 	// An animation frame 'ticks' 60 times a second (1000ms)
 	const DELAY = ms || ((1000 / 60) * 4); // Default delay is a single animation frame times 4
 	let timestamp = performance.now();
+	let running = false;
+	const onTimeOut = event => {
+		timestamp = performance.now();
+		func.call(scope, event);
+		running = false;
+	};
 	return event => {
-		requestAnimationFrame(currentEventTime => {
-			if ((currentEventTime - timestamp) >= DELAY) {
-				timestamp = currentEventTime;
-				func.call(context, event);
-			}
-		});
+		if (!running) {
+			running = true;
+			requestAnimationFrame(currentEventTime => {
+				setTimeout(onTimeOut, Math.max(0, DELAY - (currentEventTime - timestamp)), event);
+			});
+		}
 	};
 };
