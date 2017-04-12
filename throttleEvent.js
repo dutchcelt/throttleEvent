11d2a412ad471c0f341fa9c4c687112f49d727b3
Egor Kloos <studio@dutchcelt.com>
2017-04-12 15:36:33 +0200

    Refactored naming conventions

--- throttleEvent.js
+++ throttleEvent.js
@@ -1,31 +1,32 @@
 /**
- * throttleEvent
+ * throttler
  * Used to limit events that can be fired in rapid succession.
  * This can prevent an event like onresize or onscroll from thrashing the UI.
  *
- * Example: addEventListener('resize', throttleEvent(_handler, 500, this), false);
+ * Example: addEventListener('resize', throttleEvent(_handler, 500), false);
  *
- * @arg {function} func  - This is the actual event handler to be passed on
- * @arg {number} ms      - The optional delay time in milliseconds (throttling)
- * @arg {object} context - Optional scope in which to return the function handler
- * @return {function}    - The timeout function that is triggered by the event listner on each event call
+ * @arg {function} callback - This is the actual event handler to be passed on
+ * @arg {number} ms         - The optional delay time in milliseconds (throttling)
+ * @arg {object} scope      - Optional scope in which to return the function handler
+ * @return {function}       - The timeout function that is triggered by the event listner on each event call
  */
-export const throttleEvent = (func, ms, scope = this) => {
+const throttler = (callback, ms, scope = this) => {
 	// An animation frame 'ticks' 60 times a second (1000ms)
-	const DELAY = ms || ((1000 / 60) * 4); // Default delay is a single animation frame times 4
-	let timestamp = performance.now();
+	const FRAME = (1000 / 60);
+	// Default delay is a single animation frame times 4
+	const DELAY = ms || (FRAME * 4);
 	let running = false;
-	const onTimeOut = event => {
-		timestamp = performance.now();
-		func.call(scope, event);
-		running = false;
+	const onTimeOut = (...args) => {
+		requestAnimationFrame(() => {
+			callback.apply(scope, args);
+			running = false;
+		});
 	};
-	return event => {
-		if (!running) {
-			running = true;
-			requestAnimationFrame(currentEventTime => {
-				setTimeout(onTimeOut, Math.max(0, DELAY - (currentEventTime - timestamp)), event);
-			});
-		}
+	return (...args) => {
+		if (!running) running = setTimeout(onTimeOut, Math.max(0, DELAY - FRAME), ...args);
 	};
 };
+
+const throttleEvent = throttle;
+
+export { throttleEvent };
